0e5e1a6a01bad8abd03d23c5ebfdb433cda0e29f
taivt <thientai015344@gmail.com>
2022-02-27 23:19:02 +0700

    day 1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,318 @@
 
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      Hello Word
+    <div className="App-kallo">
+
+      <nav className="navbar" >Herder</nav>
+      <nav className="navbar-donw" >down_Herder</nav>
+      <div className="content-workspace">
+        <div className="item-list-ul" >
+            <header className="brainstom"> sap lam</header>
+            <ul>
+              <li>
+                    <img className="img-item-list" src="https://trello-backgrounds.s3.amazonaws.com/53baf533e697a982248cd73f/480x480/96406688eb291c869064290cfb9b0c80/shutterstock_134707556.jpg" alt=""/>
+              </li>
+              <li>
+                    work everyday
+              </li>
+              <li>
+                    work everyday
+              </li>
+              <li>
+                    work everyday
+              </li>
+              <li>
+                    work everyday
+              </li>
+              <li>
+                    work everyday
+              </li>
+              <li>
+                    work everyday
+              </li>
+              <li>
+                    work everyday
+              </li>
+              <li>
+                    work everyday
+              </li>
+              <li>
+                    work everyday
+              </li>
+              <li>
+                    work everyday
+              </li>
+              <li>
+                    work everyday
+              </li>
+              <li>
+                    work everyday
+              </li>
+              <li>
+                    work everyday
+              </li>
+              <li>
+                    work everyday
+              </li>
+            </ul>
+            <footer className="footerf"> footer add nother card </footer>
+        </div>
+        <div className="item-list-ul" >
+            <header className="brainstom"> sap lam</header>
+            <ul>
+              <li>
+                    <img className="img-item-list" src="https://trello-backgrounds.s3.amazonaws.com/53baf533e697a982248cd73f/480x480/96406688eb291c869064290cfb9b0c80/shutterstock_134707556.jpg" alt=""/>
+              </li>
+              <li>
+                    work everyday
+              </li>
+              <li>
+                    work everyday
+              </li>
+              <li>
+                    work everyday
+              </li>
+              <li>
+                    work everyday
+              </li>
+              <li>
+                    work everyday
+              </li>
+              <li>
+                    work everyday
+              </li>
+              <li>
+                    work everyday
+              </li>
+              <li>
+                    work everyday
+              </li>
+              <li>
+                    work everyday
+              </li>
+              <li>
+                    work everyday
+              </li>
+              <li>
+                    work everyday
+              </li>
+              <li>
+                    work everyday
+              </li>
+              <li>
+                    work everyday
+              </li>
+              <li>
+                    work everyday
+              </li>
+            </ul>
+            <footer className="footerf"> footer add nother card </footer>
+        </div>
+        <div className="item-list-ul" >
+            <header className="brainstom"> sap lam</header>
+            <ul>
+              <li>
+                    <img className="img-item-list" src="https://trello-backgrounds.s3.amazonaws.com/53baf533e697a982248cd73f/480x480/96406688eb291c869064290cfb9b0c80/shutterstock_134707556.jpg" alt=""/>
+              </li>
+              <li>
+                    work everyday
+              </li>
+              <li>
+                    work everyday
+              </li>
+              <li>
+                    work everyday
+              </li>
+              <li>
+                    work everyday
+              </li>
+              <li>
+                    work everyday
+              </li>
+              <li>
+                    work everyday
+              </li>
+              <li>
+                    work everyday
+              </li>
+              <li>
+                    work everyday
+              </li>
+              <li>
+                    work everyday
+              </li>
+              <li>
+                    work everyday
+              </li>
+              <li>
+                    work everyday
+              </li>
+              <li>
+                    work everyday
+              </li>
+              <li>
+                    work everyday
+              </li>
+              <li>
+                    work everyday
+              </li>
+            </ul>
+            <footer className="footerf"> footer add nother card </footer>
+        </div>
+        <div className="item-list-ul" >
+            <header className="brainstom"> sap lam</header>
+            <ul>
+              <li>
+                    <img className="img-item-list" src="https://trello-backgrounds.s3.amazonaws.com/53baf533e697a982248cd73f/480x480/96406688eb291c869064290cfb9b0c80/shutterstock_134707556.jpg" alt=""/>
+              </li>
+              <li>
+                    work everyday
+              </li>
+              <li>
+                    work everyday
+              </li>
+              <li>
+                    work everyday
+              </li>
+              <li>
+                    work everyday
+              </li>
+              <li>
+                    work everyday
+              </li>
+              <li>
+                    work everyday
+              </li>
+              <li>
+                    work everyday
+              </li>
+              <li>
+                    work everyday
+              </li>
+              <li>
+                    work everyday
+              </li>
+              <li>
+                    work everyday
+              </li>
+              <li>
+                    work everyday
+              </li>
+              <li>
+                    work everyday
+              </li>
+              <li>
+                    work everyday
+              </li>
+              <li>
+                    work everyday
+              </li>
+            </ul>
+            <footer className="footerf"> footer add nother card </footer>
+        </div>
+        <div className="item-list-ul" >
+            <header className="brainstom"> sap lam</header>
+            <ul>
+              <li>
+                    <img className="img-item-list" src="https://trello-backgrounds.s3.amazonaws.com/53baf533e697a982248cd73f/480x480/96406688eb291c869064290cfb9b0c80/shutterstock_134707556.jpg" alt=""/>
+              </li>
+              <li>
+                    work everyday
+              </li>
+              <li>
+                    work everyday
+              </li>
+              <li>
+                    work everyday
+              </li>
+              <li>
+                    work everyday
+              </li>
+              <li>
+                    work everyday
+              </li>
+              <li>
+                    work everyday
+              </li>
+              <li>
+                    work everyday
+              </li>
+              <li>
+                    work everyday
+              </li>
+              <li>
+                    work everyday
+              </li>
+              <li>
+                    work everyday
+              </li>
+              <li>
+                    work everyday
+              </li>
+              <li>
+                    work everyday
+              </li>
+              <li>
+                    work everyday
+              </li>
+              <li>
+                    work everyday
+              </li>
+            </ul>
+            <footer className="footerf"> footer add nother card </footer>
+        </div>
+        <div className="item-list-ul" >
+            <header className="brainstom"> sap lam</header>
+            <ul>
+              <li>
+                    {/* <img src="https://trello-backgrounds.s3.amazonaws.com/53baf533e697a982248cd73f/480x480/96406688eb291c869064290cfb9b0c80/shutterstock_134707556.jpg" alt=""/> */}
+              </li>
+              <li>
+                    work everyday
+              </li>
+              <li>
+                    work everyday
+              </li>
+              <li>
+                    work everyday
+              </li>
+              <li>
+                    work everyday
+              </li>
+              <li>
+                    work everyday
+              </li>
+            </ul>
+            <footer className="footerf"> footer add nother card </footer>
+        </div>
+        <div className="item-list-ul" >
+            <header className="brainstom"> sap lam</header>
+            <ul>
+              <li>
+                    {/* <img src="https://trello-backgrounds.s3.amazonaws.com/53baf533e697a982248cd73f/480x480/96406688eb291c869064290cfb9b0c80/shutterstock_134707556.jpg" alt=""/> */}
+              </li>
+              <li>
+                    work everyday
+              </li>
+              <li>
+                    work everyday
+              </li>
+              <li>
+                    work everyday
+              </li>
+              <li>
+                    work everyday
+              </li>
+              <li>
+                    work everyday
+              </li>
+            </ul>
+            <footer className="footerf"> footer add nother card </footer>
+        </div>
+
+      </div> 
     </div>
   );
 }
